f4e9a63c1f88cd539b8ed931814011de63a0eda9
SLMNFFT <37107143+SLMNFFT@users.noreply.github.com>
2025-05-15 07:15:52 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -130,7 +130,7 @@ def main():
     # Hero Section
     st.markdown("""
         <div class="header-gradient">
-            <h1 style="color: white; margin: 0;">PeePit Audiobook</h1>
+            <h1 style="color: white; margin: 0;">🎧PeePit Audiobook</h1>
             <p style="color: rgba(255,255,255,0.8); font-size: 1.1rem;">
                 Peep your doc after transforming it into immersive audio experiences
             </p>
